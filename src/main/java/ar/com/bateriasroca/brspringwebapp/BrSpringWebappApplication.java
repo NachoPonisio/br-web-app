3332b56e17c8b6dbd6589a1516773c93c54453c6
NachoPonisio <ponisio.ignacio@gmail.com>
2018-09-18 17:52:01 -0300

    Initial commit

---
+++ src/main/java/ar/com/bateriasroca/brspringwebapp/BrSpringWebappApplication.java
@@ -0,0 +1,12 @@
+package ar.com.bateriasroca.brspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrSpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrSpringWebappApplication.class, args);
+	}
+}
