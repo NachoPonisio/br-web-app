9c34406ac74f6ee01b902d94aab059b76e3a3040
NachoPonisio <ponisio.ignacio@gmail.com>
2018-09-19 00:52:58 -0300

    added static resources

---
+++ src/main/java/ar/com/bateriasroca/brspringwebapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ar.com.bateriasroca.brspringwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index.html"})
+    public String getIndex(){
+        return "index";
+    }
+}
